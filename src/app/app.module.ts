322a40561b968ca5f99aa8354a999a95b2bb92eb
Dhairya Shelar <dhairya.shelar@drcsystems.in>
2021-09-02 12:28:39 +0530

    Added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './modules/shared.module';
 import { HeaderComponent } from './common/header/header.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 // import { AgeCalculatorComponent } from './components/age-calculator/age-calculator.component';
 // import { BasicCalculatorComponent } from './components/basic-calculator/basic-calculator.component';
 // import { BmiCalculatorComponent } from './components/bmi-calculator/bmi-calculator.component';
@@ -22,7 +24,13 @@ import { HeaderComponent } from './common/header/header.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
